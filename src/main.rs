05f6835a22689e8107c4ebb07365a569bf4e1e02
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-08-10 14:51:20 -0700

    Train the model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use rustlearn::cross_validation::CrossValidation;
 use rustlearn::feature_extraction::DictVectorizer;
+use rustlearn::linear_models::sgdclassifier;
+use rustlearn::metrics::accuracy_score;
 use rustlearn::prelude::*;
 use std::io::Read;
+use std::time::Instant;
 
 // Convert input data to feature matrix
 fn parse(data: &str) -> (SparseRowArray, Array) {
@@ -40,6 +44,42 @@ fn download_data(url: &str) -> String {
     data
 }
 
+fn fit(x: &SparseRowArray, y: &Array) -> (f32, f32) {
+    let num_epochs = 10;
+    let num_folds = 10;
+
+    let mut test_accuracy = 0.0;
+    let mut train_accuracy = 0.0;
+
+    for (train_indicies, test_indices) in CrossValidation::new(y.rows(), num_folds) {
+        let x_train = x.get_rows(&train_indicies);
+        let x_test = x.get_rows(&test_indices);
+
+        let y_train = y.get_rows(&train_indicies);
+        let y_test = y.get_rows(&test_indices);
+
+        let mut model = sgdclassifier::Hyperparameters::new(x.cols())
+            .learning_rate(0.05)
+            .l2_penalty(0.01)
+            .build();
+
+        for _ in 0..num_epochs {
+            model.fit(&x_train, &y_train).unwrap();
+        }
+
+        let fold_test_accuracy = accuracy_score(&y_test, &model.predict(&x_test).unwrap());
+        let fold_train_accuracy = accuracy_score(&y_train, &model.predict(&x_train).unwrap());
+
+        test_accuracy += fold_test_accuracy;
+        train_accuracy += fold_train_accuracy;
+    }
+
+    (
+        test_accuracy / num_folds as f32,
+        train_accuracy / num_folds as f32,
+    )
+}
+
 fn main() {
     let data = download_data(
         "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip",
@@ -51,5 +91,14 @@ fn main() {
         x.cols(),
         x.nnz(),
         y.mean() * 100.0
+    );
+    let now = Instant::now();
+    // let start_time = precise_time_ns();
+    let (test_accuracy, train_accuracy) = fit(&x, &y);
+    // let duration = precise_time_ns() - start_time;
+    println!("Training time: {:?}", now.elapsed());
+    println!(
+        "Test accuracy: {:.3}, train accuracy: {:.3}",
+        test_accuracy, train_accuracy
     );
 }
