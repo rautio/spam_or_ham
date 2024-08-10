f56508fe265002a2bc76dd513ab5ec5f7dbd03d4
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-08-10 14:26:42 -0700

    Unzip data

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,25 @@
-fn main() {
-    let zipped = download_data(
-        "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip",
-    );
-    println!("Downloaded {} bytes of data", zipped.len())
+use std::io::{Cursor, Read};
+use zip::read::ZipArchive;
+
+fn download_data(url: &str) -> String {
+    let mut tmpfile = tempfile::tempfile().unwrap();
+    let _ = reqwest::blocking::get(url).unwrap().copy_to(&mut tmpfile);
+    let mut zip = zip::ZipArchive::new(tmpfile).unwrap();
+    let mut file_zip = zip.by_name("SMSSpamCollection").unwrap();
+
+    let mut data = String::new();
+    file_zip.read_to_string(&mut data).unwrap();
+
+    data
 }
 
-fn download_data(url: &str) -> Vec<u8> {
-    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-    response.into_bytes()
+fn main() {
+    let data = download_data(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip",
+    );
+    println!("Downloaded {} lines", data.len());
+
+    for line in data.lines().take(3) {
+        println!("{}", line);
+    }
 }
