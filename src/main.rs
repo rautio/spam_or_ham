dc7de05c92989a6517bb1248e6e649e3006e0bf2
Oskari Rautiainen <oskari.rautiainen@gmail.com>
2024-08-10 13:54:02 -0700

    Add ability to download training data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let zipped = download_data(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/00228/smsspamcollection.zip",
+    );
+    println!("Downloaded {} bytes of data", zipped.len())
+}
+
+fn download_data(url: &str) -> Vec<u8> {
+    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
+    response.into_bytes()
 }
